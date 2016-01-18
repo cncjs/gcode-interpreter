b568d614b99cd6d68c1af48287d509b98c4c2f9e
Cheton Wu <cheton@gmail.com>
2016-01-18 11:03:36 +0800

    Updare index.js

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 Object.defineProperty(exports, "__esModule", {
     value: true
@@ -92,7 +92,7 @@ var interpret = function interpret(self, data) {
     });
 };
 
-var GCodeInterpreter = (function () {
+var GCodeInterpreter = function () {
     function GCodeInterpreter(options) {
         _classCallCheck(this, GCodeInterpreter);
 
@@ -164,6 +164,6 @@ var GCodeInterpreter = (function () {
     }]);
 
     return GCodeInterpreter;
-})();
+}();
 
 exports.GCodeInterpreter = GCodeInterpreter;
